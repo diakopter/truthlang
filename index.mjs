ecd65561da2a32298cfca64a19aaf6d48981972f
diakopter <diakopter@gmail.com>
2018-08-11 17:28:34 -0400

    .

--- index.mjs
+++ index.mjs
@@ -1,4 +1,13 @@
+var pretty = require('pretty-time');
 
 import {entryPoint} from "./main.wasm";
 
-console.log(entryPoint(1, 2));
+var timeit = (func) => {
+    var start = process.hrtime();
+    var result = func();
+    console.log("Elapsed: " + pretty(process.hrtime(start)));
+    return result;
+};
+
+console.log("Result: " + timeit(() => {return entryPoint(4, 4)}));
+
