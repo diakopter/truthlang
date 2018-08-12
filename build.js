27037994b95546d92957f2313bf4326823a8d682
diakopter <diakopter@gmail.com>
2018-08-11 21:47:32 -0400

    .

--- build.js
+++ build.js
@@ -2,18 +2,22 @@ const { readFileSync, writeFileSync } = require("fs");
 const wabt = require("wabt");
 const path = require("path");
 const cpp = require("./cpp.js");
+const sleep = require('system-sleep');
 
 var wat_processed = "not_done";
 var pp_error = "";
 
 const pp = cpp.create( { 
     signal_char : '#',
-    warn_func : null,
-    error_func : null,
+//    warn_func : null,
+    error_func : function(error) {
+        throw "got pp error: " + error;
+    },
     include_func : function(file, is_global, resumer, error) {
         // `is_global` is `true` if the file name was enclosed
         // in < .. > rather than " .. ".
         const contents = readFileSync(file, "utf8");
+        console.log("got contents: " + contents);
         if (contents === null) {
             pp_error = "could not access " + file + ":\n" + error;
             wat_processed = "errored";
@@ -29,13 +33,18 @@ const pp = cpp.create( {
     comment_stripper : null
  } );
 
-const inputWat = "truth.wat";
+const startWat = "truth.wat";
 
+console.log("got 1");
+const inputSrc = readFileSync(startWat, "utf8");
+console.log("got startsrc " + inputSrc);
+var source = pp.run(inputSrc);
+console.log('got source' + source);
 while (wat_processed === "not_done") {
-    await new Promise(done => setTimeout(done, 500));
+    
 }
 if (wat_processed === "errored") {
-    throw pp_error;
+    throw "errored: " + pp_error;
 }
 
 writeFileSync("main.wat", wat_processed); // cache output for debugging/fun
